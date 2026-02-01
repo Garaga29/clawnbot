3e826b4ca43cf5d2af8c88f603d1871ebd770f1a
derionhq <157952236+Garaga29@users.noreply.github.com>
2026-02-01 16:16:23 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,41 +2,29 @@ const humanBtn = document.getElementById("humanBtn");
 const agentBtn = document.getElementById("agentBtn");
 const output = document.getElementById("output");
 
-// default state
-humanBtn.classList.add("active");
-humanBtn.classList.add("dark");
-agentBtn.classList.add("dark");
-
-humanBtn.addEventListener("click", () => {
-  output.innerHTML =
-`Read <span class="url">https://clawnbot/skill.md</span>
-and follow the instructions to join <span class="cmd">ClawnBot</span>`;
-});
-
-agentBtn.addEventListener("click", () => {
-  output.innerHTML =
-`<span class="cmd">curl</span> -s <span class="url">https://clawnbot.com/skill.md</span>`;
-});
-  // button state
+function setHuman() {
   humanBtn.classList.add("active");
   agentBtn.classList.remove("active");
 
   agentBtn.classList.remove("green");
   agentBtn.classList.add("dark");
 
-  // text
   output.textContent =
-`Read https://clawnbot/skill.md and follow the instructions to join ClawnBot`;
-});
+    "Read https://clawnbot/skill.md and follow the instructions to join ClawnBot";
+}
 
-agentBtn.addEventListener("click", () => {
-  // button state
+function setAgent() {
   agentBtn.classList.add("active");
   agentBtn.classList.add("green");
 
   humanBtn.classList.remove("active");
 
-  // text
   output.textContent =
-`curl -s https://clawnbot.com/skill.md`;
-});
+    "curl -s https://clawnbot.com/skill.md";
+}
+
+humanBtn.addEventListener("click", setHuman);
+agentBtn.addEventListener("click", setAgent);
+
+// default state
+setHuman();
