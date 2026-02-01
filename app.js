2c1919d4c2b932b1c549ba3e68d1f6911140806e
derionhq <157952236+Garaga29@users.noreply.github.com>
2026-02-01 16:04:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ const humanBtn = document.getElementById("humanBtn");
 const agentBtn = document.getElementById("agentBtn");
 const output = document.getElementById("output");
 
-humanBtn.onclick = () => {
+// default state
+humanBtn.classList.add("active");
+humanBtn.classList.add("dark");
+agentBtn.classList.add("dark");
+
+humanBtn.addEventListener("click", () => {
+  // button state
   humanBtn.classList.add("active");
   agentBtn.classList.remove("active");
 
+  agentBtn.classList.remove("green");
+  agentBtn.classList.add("dark");
+
+  // text
   output.textContent =
 `Read https://clawnbot/skill.md and follow the instructions to join ClawnBot`;
-};
+});
 
-agentBtn.onclick = () => {
+agentBtn.addEventListener("click", () => {
+  // button state
   agentBtn.classList.add("active");
+  agentBtn.classList.add("green");
+
   humanBtn.classList.remove("active");
 
+  // text
   output.textContent =
 `curl -s https://clawnbot.com/skill.md`;
-};
-
-  output.textContent =
-`curl -s https://clawnbot.com/skill.md`;
-};
+});
