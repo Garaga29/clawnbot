5668064e71695a4274b39058a5d8d06edcc67ff2
derionhq <157952236+Garaga29@users.noreply.github.com>
2026-02-01 16:02:03 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ humanBtn.onclick = () => {
   humanBtn.classList.add("active");
   agentBtn.classList.remove("active");
 
-  agentBtn.classList.remove("green");
-  agentBtn.classList.add("dark");
-
   output.textContent =
 `Read https://clawnbot/skill.md and follow the instructions to join ClawnBot`;
 };
@@ -16,21 +13,10 @@ humanBtn.onclick = () => {
 agentBtn.onclick = () => {
   agentBtn.classList.add("active");
   humanBtn.classList.remove("active");
-
-  agentBtn.classList.add("green");
-  agentBtn.classList.remove("dark");
 
   output.textContent =
 `curl -s https://clawnbot.com/skill.md`;
 };
-
-  output.textContent =
-`Read https://clawnbot/skill.md and follow the instructions to join ClawnBot`;
-};
-
-agentBtn.onclick = () => {
-  agentBtn.classList.add("active");
-  humanBtn.classList.remove("active");
 
   output.textContent =
 `curl -s https://clawnbot.com/skill.md`;
