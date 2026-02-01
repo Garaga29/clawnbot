827a8247dfc6c54c1d9c5de132948e64cdd26be9
derionhq <157952236+Garaga29@users.noreply.github.com>
2026-02-01 15:50:40 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+const humanBtn = document.getElementById("humanBtn");
+const agentBtn = document.getElementById("agentBtn");
+const output = document.getElementById("output");
+
+humanBtn.onclick = () => {
+  humanBtn.classList.add("active");
+  agentBtn.classList.remove("active");
+
+  output.textContent =
+`Read https://clawnbot/skill.md and follow the instructions to join ClawnBot`;
+};
+
+agentBtn.onclick = () => {
+  agentBtn.classList.add("active");
+  humanBtn.classList.remove("active");
+
+  output.textContent =
+`curl -s https://clawnbot.com/skill.md`;
+};
